824cbc5c2a664999ed4abb638cb34b9b47e5a40e
Christian Baumgartner <treegartner@gmail.com>
2015-06-22 15:24:19 +0200

    added infected-resources script

---
+++ infected-resources/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/cumulodev/nimbusec"
+)
+
+func main() {
+	filter := flag.String("filter", "severity ge 3 and (event eq \"malware\" or event eq \"webshell\")", "filter for when a domain is considered infected")
+	domain := flag.String("domain", "ALL", "define specific domain or ALL to lookup over all domains and resources")
+	url := flag.String("url", nimbusec.DefaultAPI, "url to nimbusec API")
+	key := flag.String("key", "", "nimbusec API key")
+	secret := flag.String("secret", "", "nimbusec API secret")
+	flag.Parse()
+
+	//TODO: validate parameters
+
+	api, err := nimbusec.NewAPI(*url, *key, *secret)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// find infected domains
+	var domains []nimbusec.Domain
+	if *domain != "ALL" {
+		obj, err := api.GetDomainByName(*domain)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		domains = []nimbusec.Domain{*obj}
+	} else {
+		domains, err = api.FindInfected(*filter)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// fetch resources per domain
+	for _, domain := range domains {
+		results, err := api.FindResults(domain.Id, *filter)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, result := range results {
+			fmt.Printf("%s,%s\n", domain.Name, result.Resource)
+		}
+	}
+
+}
