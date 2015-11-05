808372e6a5a8f49a6e6486e4e20bf30f77afee61
Thomas Kastner <tom@sprungknoedl.at>
2015-11-05 10:41:13 +0100

    ability to run paralell jobs with sync-domains, safer defaults

--- sync-domains/main.go
+++ sync-domains/main.go
@@ -7,90 +7,124 @@ import (
 	"log"
 	"os"
 
+	"github.com/cumulodev/goutils/pool"
 	"github.com/cumulodev/nimbusec"
 )
 
 func main() {
-
-	apiUrlPtr := flag.String("url", nimbusec.DefaultAPI, "API Url")
-	apiKeyPtr := flag.String("key", "abc", "API key for authentication")
-	apiSecretPtr := flag.String("secret", "abc", "API secret for authentication")
-	filePtr := flag.String("file", "import.csv", "path to import file")
-	shouldDelete := flag.Bool("delete", true, "delete domains from nimbusec if not provided in the CSV")
-	updatePtr := flag.Bool("update", true, "updates domain info; false to just insert new domains")
+	url := flag.String("url", nimbusec.DefaultAPI, "API Url")
+	key := flag.String("key", "abc", "API key for authentication")
+	secret := flag.String("secret", "abc", "API secret for authentication")
+	file := flag.String("file", "import.csv", "path to import file")
+	delete := flag.Bool("delete", false, "delete domains from nimbusec if not provided in the CSV")
+	update := flag.Bool("update", false, "updates domain info; false to just insert new domains")
+	workers := flag.Int("workers", 1, "number of paralell workers (please do not use too many workers)")
 	flag.Parse()
 
-	api, err := nimbusec.NewAPI(*apiUrlPtr, *apiKeyPtr, *apiSecretPtr)
+	// creates a new nimbusec API instance
+	api, err := nimbusec.NewAPI(*url, *key, *secret)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	/*
-	 * READ CSV FILE
-	 */
-	importfile, err := os.Open(*filePtr)
+	// open csv input file and parse it
+	fh, err := os.Open(*file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer importfile.Close()
-	reader := csv.NewReader(importfile)
+	defer fh.Close()
+	reader := csv.NewReader(fh)
 	reader.FieldsPerRecord = -1 // see the Reader struct information below
-	rawCSVdata, err := reader.ReadAll()
+	rows, err := reader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// BUILD MAP WITH NEW DOMAINS
+	pool := pool.New(*workers)
+	pool.Start()
+
+	// keep track of domain names (required for delete step later)
 	ref := make(map[string]struct{})
 
-	for _, each := range rawCSVdata {
-		url := each[0]
-		scheme := each[2]
-		bundle := each[3]
+	for _, row := range rows {
+		name := row[0]
+		scheme := row[2]
+		bundle := row[3]
 
-		// BUILD REF
-		ref[url] = struct{}{}
-
-		// ADD DOMAIN TO SET
+		// construct domain
 		domain := &nimbusec.Domain{
-			Name:      url,
+			Name:      name,
 			Bundle:    bundle,
 			Scheme:    scheme,
-			DeepScan:  scheme + "://" + url,
-			FastScans: []string{scheme + "://" + url},
+			DeepScan:  scheme + "://" + name,
+			FastScans: []string{scheme + "://" + name},
 		}
 
-		// UPSERT DOMAIN
-		fmt.Printf("UPSERT DOMAIN: %+v\n", domain)
-		if *updatePtr {
-			_, err := api.CreateOrUpdateDomain(domain)
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			_, err := api.CreateOrGetDomain(domain)
-			if err != nil {
-				log.Fatal(err)
+		// upsert domain
+		ref[name] = struct{}{}
+		pool.Add(upsertJob{
+			api:    api,
+			domain: domain,
+			update: *update,
+		})
+	}
+
+	pool.Wait()
+
+	// sync
+	// delete domains not listed in new set
+	if *delete {
+		// read all domains from api
+		domains, err := api.FindDomains(nimbusec.EmptyFilter)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// cross reference domains in nimbusec with csv file and delete all
+		// domains not present in csv file
+		for _, domain := range domains {
+			if _, ok := ref[domain.Name]; !ok {
+				pool.Add(deleteJob{
+					api:    api,
+					domain: &domain,
+				})
 			}
 		}
 
+		pool.Wait()
 	}
+}
 
-	// READ ALL DOMAINS FROM API
-	currDomains, err := api.FindDomains(nimbusec.EmptyFilter)
-	if err != nil {
-		log.Fatal(err)
-	}
+type upsertJob struct {
+	api    *nimbusec.API
+	domain *nimbusec.Domain
+	update bool
+}
 
-	// SYNC
-	// DELETE DOMAINS NOT LISTED IN NEW SET
-	if *shouldDelete {
-		for _, d := range currDomains {
-			if _, ok := ref[d.Name]; !ok {
-				fmt.Printf("DELETE DOMAIN: %s\n", d.Name)
-				api.DeleteDomain(&d, true)
-			}
+func (this upsertJob) Work() {
+	fmt.Printf("upsert domain: %+v\n", this.domain)
+	if this.update {
+		if _, err := this.api.CreateOrUpdateDomain(this.domain); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if _, err := this.api.CreateOrGetDomain(this.domain); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
+
+func (this upsertJob) Save() {}
+
+type deleteJob struct {
+	api    *nimbusec.API
+	domain *nimbusec.Domain
+}
+
+func (this deleteJob) Work() {
+	fmt.Printf("delete domain: %s\n", this.domain.Name)
+	this.api.DeleteDomain(this.domain, true)
+}
+
+func (this deleteJob) Save() {}
